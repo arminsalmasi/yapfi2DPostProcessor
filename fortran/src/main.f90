40cd430ce73ee4cd7b73f08d25f8fa4a5963f88b
arminsalmasi <arminsalmasi@gmail.com>
2018-05-28 14:50:37 +0200

    commit 13 fine tune before merge

--- fortran/src/main.f90
+++ fortran/src/main.f90
@@ -1,6 +1,6 @@
   PROGRAM YAPFIPost
 
-  USE mdsPostProcessor
+  USE mdsPostProcessor, 
   USE mGetInput
   USE mDoProcess
   USE mSetVtk
@@ -15,20 +15,14 @@
   READ(*,*) tyParrent%sInPath
   WRITE(*,*) "Outout directory path  (Put '\' at the end):"
   READ(*,*) tyParrent%sOutPath
-
   !tyParrent%sInPath ='C:\USErs\salmasi\DOcuments\Mycodes\yapfiPP\test1D\'         !temporary
   !tyParrent%sOutPath ='C:\USErs\salmasi\DOcuments\Mycodes\yapfiPP\test1D\vtk\'    !temporary
   WRITE(*,*) '** Reading the data'
-
   iFunctionReturnValue = fGetRawData(tyParrent)
-
   WRITE(*,*) '** Processing the data'
   iFunctionReturnValue = fDoProcessData(tyParrent)
-
-
   WRITE(*,*) '** writing to output files'
   iFunctionReturnValue = fSetVtkFile(tyParrent)
-
   WRITE(*,*) 'End of process, press return!'
   READ(*,*)
   DEALLOCATE(tyParrent)
